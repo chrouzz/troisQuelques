9f7ea720651cd36bac82023614fc23c6c57b4961
pottier <chrouzz@gmail.com>
2014-06-03 17:52:16 +0800

    Fixed details

--- web.js
+++ web.js
@@ -13,16 +13,16 @@ var configDB = require('./config/database.js');
 
 var app = express();
 
-//var html = jade.renderFile('views/index.jade');
 
 // configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
+var mongo = mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 var env = process.env.NODE_ENV || 'development';
 
-if ('development' == env) {
+if (env.indexOf('development') !== -1) {
+    console.log('here');
 
 	// set up our express application
 	app.use(logger('dev')); // log every request to the console
@@ -30,10 +30,10 @@ if ('development' == env) {
 	app.use(bodyParser()); // get information from html forms
 	app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
-app.use(function (req, res, next) {
-    console.log(req.body) // populated!
-    next()
-})
+    app.use(function (req, res, next) {
+        console.log(req.body) // populated!
+        next()
+    })
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade'); // set up ejs for templating
 
